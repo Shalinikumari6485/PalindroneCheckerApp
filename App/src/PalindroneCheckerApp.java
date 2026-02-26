1cef47075e5afd7f65fd6f55e3e0d1c56aa71a95
Shalinikumari6485 <shalinikumari1044@gmail.com>
2026-02-26 23:32:39 +0530

    [Shalini] Added UC3-PalindroneStringChecking

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -5,5 +5,23 @@ public class PalindroneCheckerApp {
     public static void main(String[] args){
         System.out.println("Welcome to Palindrone Checker App Management System");
         System.out.println("Modify this logic to usecase 2");
+        System.out.println("Enter a string(lower case):");
+        String sc = sc.nextLine();
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < sc.length() / 2; i++) {
+            if (sc.charAt(i) != sc.charAt(sc.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not a Palindrome");
+        }
+
+        sc.close();
     }
 }
