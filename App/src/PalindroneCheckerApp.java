8a2a1660497dc550965b2b0facf7e2aeef02877a
Shalinikumari6485 <shalinikumari1044@gmail.com>
2026-02-26 23:54:38 +0530

    [Shalini] Added UC6-QueueAndStackBasedCheck

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -3,41 +3,41 @@
 //useCase 1: Welcome page
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
+
                 System.out.println("Enter a string(lower case):");
         s=sc.nextLine();
 
-        char [] chars= s.toCharArray();
+        Queue<Character> queue = new LinkedList<>();
         Stack<Character> stack = new Stack<>();
 
-        int start =0;
-        int end=chars.length - 1;
         // Push each character of the string into the stack
         for (char c : s.toCharArray()) {
+            queue.add(c);
             stack.push(c);
         }
 
-        boolean isPalindrome=true;
-
+        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (chars[start] != chars[end]) {
-
-                for (char c : s.toCharArray()) {
-                    if (c != stack.pop()) {
+        // Iterate again through original string
+        for (char c : s.toCharArray()) {
+            if (c != stack.pop()) {
+                while (!queue.isEmpty()) {
+                    if (queue.remove() != stack.pop()) {
                         isPalindrome = false;
                         break;
                     }
-                    start++;
-                    end--;
                 }
 
-                if (isPalindrome) {
+                if(isPalindrome){
                     System.out.println("Palindrome");
-                } else {
+                }
+                else{
                     System.out.println("Is not a Palindrome");
                 }
                 sc.close();
