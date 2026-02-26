7f47c63507828707fcf82a3292f634dc21631720
Shalinikumari6485 <shalinikumari1044@gmail.com>
2026-02-26 23:02:57 +0530

    [Shalini] Added UC2-HardCodePalindrone

--- App/src/PalindroneCheckerApp.java
+++ App/src/PalindroneCheckerApp.java
@@ -3,6 +3,7 @@
 //useCase 1: Welcome page
 public class PalindroneCheckerApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Palindrone Checker App Management System")
+        System.out.println("Welcome to Palindrone Checker App Management System");
+        System.out.println("Modify this logic to usecase 2");
     }
 }
